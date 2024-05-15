62fd0cc802b0822a2297854aa1a9174b55bc45cb
Rishi Mishra <heaven@Rishis-MacBook-Air.local>
2024-05-15 16:36:41 +0530

    feat: appilcation context

--- src/main/java/com/example/Second/App.java
+++ src/main/java/com/example/Second/App.java
@@ -10,8 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-    	BeanFactory factory=new XmlBeanFactory(new FileSystemResource("spring.xml"));
+//    	BeanFactory factory=new XmlBeanFactory(new FileSystemResource("spring.xml"));
     		
+    	ApplicationContext factory=new ClassPathXmlApplicationContext("spring.xml");
         Alien obj=(Alien)factory.getBean("alien");
         obj.code();
     }
