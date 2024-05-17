b2970fe31da48bd3d6ef133a86e1ecf5dda0cbaf
Rishi Mishra <heaven@Rishis-MacBook-Air.local>
2024-05-17 06:15:01 +0530

    feat: table is missing from h2(in memory database)

--- src/main/java/com/example/demo/repository/AlienRepo.java
+++ src/main/java/com/example/demo/repository/AlienRepo.java
@@ -3,18 +3,32 @@ package com.example.demo.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Alien;
 
 @Repository
 public class AlienRepo {
+    @Autowired
+    private JdbcTemplate template;
 
     public void add(Alien alien) {
         System.out.println("Added");
+        String sql = "insert into alien (id,name,tech) values (?,?,?)";
+        template.update(sql, alien.getId(), alien.getName(), alien.getTech());
     }
 
-    public List<Alien> findAll(){
+    public List<Alien> findAll() {
         return new ArrayList<Alien>();
+    }
+
+    public JdbcTemplate getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(JdbcTemplate template) {
+        this.template = template;
     }
 }
